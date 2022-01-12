f24eb35c3c2fb5555990e9e5abf270942edf46fd
Miller Juma <miller.developer.william@gmail.com>
2022-01-12 09:45:34 +0300

    complete app

---
+++ connect.php
@@ -0,0 +1,4 @@
+<?php 
+	$con = mysqli_connect("localhost", "root","@Mj37055765");
+	mysqli_select_db($con, "ita");
+?>
---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\get_client_ip_env\u0028\u0029",
+            "name": "get_client_ip_env",
+            "summary": "",
+            "url": "namespaces/default.html#function_get_client_ip_env"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
